f0e7f1a85e4274896b83b45e8f334b0e3a2cd27b
charle0911 <charle0911@gmail.com>
2019-10-09 00:26:03 +0800

    Add sub tree of another tree

---
+++ src/com/main/leetcode/tree/SubtreeofAnotherTree.java
@@ -0,0 +1,34 @@
+package com.main.leetcode.tree;
+
+public class SubtreeofAnotherTree {
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        return isSubTreeWithRoot(s, t) || isSubtree(s.left, t.left) || isSubtree(s.right, t.right);
+    }
+
+    private boolean isSubTreeWithRoot(TreeNode s, TreeNode t) {
+        if (s == null && t == null)
+            return true;
+        if (s == null || t == null)
+            return false;
+        if (s.val != t.val)
+            return false;
+        return isSubTreeWithRoot(s.left, t.left) || isSubTreeWithRoot(s.right, t.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode input1 = new TreeNode(3);
+        input1.left = new TreeNode(4);
+        input1.right = new TreeNode(5);
+        input1.left.left = new TreeNode(1);
+        input1.left.right = new TreeNode(2);
+
+        TreeNode input2 = new TreeNode(4);
+        input2.left = new TreeNode(1);
+        input2.left.right = new TreeNode(2);
+
+        SubtreeofAnotherTree solution = new SubtreeofAnotherTree();
+        assert !solution.isSubtree(input1, input2);
+
+    }
+}
