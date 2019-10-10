f8a8a55f835a13b3d8b74c82121d012afbe7ce63
charle0911 <charle0911@gmail.com>
2019-10-11 02:01:11 +0800

    Add mini depth of binart tree

---
+++ src/com/main/leetcode/tree/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,24 @@
+package com.main.leetcode.tree;
+
+public class MinimumDepthOfBinaryTree {
+    public int minDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        return Math.min(left, right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode input1 = new TreeNode(3);
+        input1.left = new TreeNode(9);
+        input1.right = new TreeNode(20);
+        input1.right.left = new TreeNode(15);
+        input1.right.right = new TreeNode(7);
+
+        MinimumDepthOfBinaryTree solution = new MinimumDepthOfBinaryTree();
+
+        assert solution.minDepth(input1) != 2;
+    }
+}
